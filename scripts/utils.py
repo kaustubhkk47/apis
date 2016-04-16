7ab40bcd64cc1232bb534d01a2900bb18f9376a0
kaustubhkk47 <kausthegreat@gmail.com>
2016-04-16 07:17:31 +0530

    finally worked

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.db import connection
 
 def closeDBConnection():
     connection.close()
--- urls.py
+++ urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from urlHandlers.catalog import *
+from urlHandlers.catalog_handler import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
