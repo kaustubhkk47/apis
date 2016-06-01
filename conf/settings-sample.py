e237f42741311e67fcec0f82640e62dca6b74910
Kaustubh <kausthegreat@gmail.com>
2016-06-01 16:31:01 +0530

    Changed settings for deployment

---
+++ conf/settings-sample.py
@@ -0,0 +1,33 @@
+## Secret Key
+
+SECRET_KEY = ''
+
+## Database
+
+DATABASES = {
+    
+}
+
+BASE_WEBAPP_URL = ""
+
+##Static
+
+STATIC_ROOT = ""
+STATIC_URL = ''
+STATICFILES_DIRS = (
+    
+)
+
+## Media
+
+MEDIA_ROOT = ""
+MEDIA_URL = ''
+
+## Email Backend
+
+EMAIL_BACKEND = ''
+AWS_SES_ACCESS_KEY_ID = ""
+AWS_SES_SECRET_ACCESS_KEY = ""
+AWS_SES_REGION_NAME = ''
+AWS_SES_REGION_ENDPOINT = ''
+
--- leads/admin.py
+++ leads/admin.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 
 from .models.buyerLeads import *
 from .models.contactUsLead import *
+from .models.sellerLeads import *
 
 admin.site.register(BuyerLeads)
 admin.site.register(ContactUsLead)
+admin.site.register(SellerLeads)
+
