33e47f85ca48d4f526fffcf3d1e6c0cd5abb276f
Kaustubh <kausthegreat@gmail.com>
2016-06-03 17:45:07 +0530

    Leads buyer mail image and link added

--- leads/views/buyerLeads.py
+++ leads/views/buyerLeads.py
@@ -68,6 +68,10 @@ def post_new_buyer_lead(request):
 
 			if newBuyerLead.product_id != None:
 				mail_dict["product_name"] = newBuyerLead.product.display_name
+				imageLink = "http://api.wholdus.com/" + newBuyerLead.product.image_path + "400x400/" + newBuyerLead.product.image_name + "-1.jpg"
+				productLink = "http://www.wholdus.com/" + newBuyerLead.product.category.slug + "-" + str(newBuyerLead.product.category_id) + "/" +newBuyerLead.product.slug +"-" + str(newBuyerLead.product.id)
+				mail_dict["product_link"] = productLink
+				mail_dict["image_link"] = imageLink
 
 			if newBuyerLead.category_id != None:
 				mail_dict["category_name"] = newBuyerLead.category.display_name
