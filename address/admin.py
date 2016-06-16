273eb79357759ebe37e14b607878e04e372ba97e
Kaustubh <kausthegreat@gmail.com>
2016-06-16 16:23:17 +0530

    address app added

--- address/admin.py
+++ address/admin.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 
 from.models.state import State
 from.models.pincode import Pincode
+from.models.city import City
+from.models.country import Country
 
 admin.site.register(State)
 admin.site.register(Pincode)
+admin.site.register(City)
+admin.site.register(Country)
---
+++ address/models/city.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from .state import State
+
+class City(models.Model):
+
+	state = models.ForeignKey(State,blank=True, null=True)
+
+	name = models.CharField(max_length=50, blank=False)
+	state_name = models.CharField(max_length=50, blank=False)
+
+	def __unicode__(self):
+		return self.name
