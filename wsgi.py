9b67885150b22d140696d28a2da7b3e01bb8791a
Aditya Jha <adityaiitr@hotmail.com>
2016-09-29 07:08:57 +0000

    azure necessary changes

--- wsgi.py
+++ wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append(os.path.dirname(__file__))
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-application = get_wsgi_application()
+application = get_wsgi_application()	
+
