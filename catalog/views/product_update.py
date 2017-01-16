2968bdf955bdd1573063f44d56de9fec9ebfcabd
Kaustubh <kausthegreat@gmail.com>
2017-01-16 14:35:18 +0530

    test buyer product responses removed from product score calculations

--- catalog/views/product_update.py
+++ catalog/views/product_update.py
@@ -2,11 +2,14 @@ from django.db import connection
 from django.utils import timezone
 from catalog.models.product import Product
 from django.db.models import Sum
+from users.models.buyer import Buyer
 
 def update_product_likes_dislikes():
  	cursor = connection.cursor()
  	nowTime = "'" + timezone.now().strftime("%Y-%m-%d %H:%M:%S") + "'"
- 	cursor.execute("UPDATE catalog_product cp JOIN (SELECT product_id, SUM(IF(response_code = 1, 1, 0)) as likes, SUM(IF(response_code = 2, 1, 0)) as dislikes FROM users_buyerproductresponse WHERE 1 GROUP BY product_id) AS temp ON cp.id = temp.product_id SET cp.product_likes = temp.likes, cp.product_dislikes = temp.dislikes, updated_at = " + nowTime)
+ 	testBuyers = Buyer.objects.filter(test_buyer=1).values_list('id', flat=True)
+ 	testBuyerIDs = ','.join(map(str, testBuyers))
+ 	cursor.execute("UPDATE catalog_product cp JOIN (SELECT product_id, SUM(IF(response_code = 1, 1, 0)) as likes, SUM(IF(response_code = 2, 1, 0)) as dislikes FROM users_buyerproductresponse WHERE buyer_id NOT IN ( " + testBuyerIDs + ") GROUP BY product_id) AS temp ON cp.id = temp.product_id SET cp.product_likes = temp.likes, cp.product_dislikes = temp.dislikes, updated_at = " + nowTime)
  	cursor.close()
 
 def update_scores():
